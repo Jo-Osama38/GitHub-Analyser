f9e1e77acb6b476f14efbd19377599bde100589a
Youssef <youssefosamasalah51@gmail.com>
2026-06-21 14:18:41 +0300

    I've finished fecthing the main user data from the GitHub API The part that took the most time was handling repos data. I used a for loop in flask to process the repos and pass them to the template then used Jinja's for loop in the HTML to dynamically display each repos on the page It took some experimenting to get eveything working the way i wanted but i learned a lot from the process

--- app.py
+++ app.py
@@ -9,24 +9,51 @@ app = Flask(__name__)
 @app.route("/home", methods = ["POST","GET"])
 def home ():
     username = None
-    data = None
     name =  None
     repos = None
     message = None
+    url_img = None
+    bio = None
+    followers = None
+    following = None
+    name_repo = None
+    names_repos = []
+
 
     if request.method == "POST":
         username = request.form.get("username")
         url = f"https://api.github.com/users/{username}"
+        url_repos = f"https://api.github.com/users/{username}/repos" 
         respose= requests.get(url)
         data = respose.json()
-        if requests.status_codes == 200:
+        if respose.status_code == 200:
+            repositories= requests.get(url_repos)
+            data_repos = repositories.json()
+            url_img =data ["avatar_url"]
             name = data["login"]
             repos = data["public_repos"]
-        elif requests.status_codes == 404:
+            bio = data["bio"]
+            followers = data["followers"]
+            following = data["following"]
+            num = 0
+            for i in range(min(5,len(data_repos))):
+                name_repo = data_repos[num]["name"]
+                description = data_repos[num]['description']
+                Top_language= data_repos[num]['language']
+                forks= data_repos[num]['forks']
+                names_repos.append({ "name" :f"{i+1}. {name_repo}","description":f"Description : {description}"
+                                    ,"Top_language":f"Top_language:{Top_language}","forks":f"Forks : {forks}"})
+                num += 1
+
+
+
+
+        elif respose.status_code == 404:
             message = "The User Name Not Found"
         
-    return render_template('index.html' ,username = username ,data = data , name = name ,repos = repos 
-                           ,message = message)
+    return render_template('index.html', name = name ,repos = repos , url_img = url_img
+                           ,message = message , bio = bio , followers =followers ,following=following
+                           ,names_repos = names_repos )
 
 
 
